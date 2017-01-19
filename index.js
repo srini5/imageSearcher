aa1e674af625b572ac97442848c0324cb2d83907
Srini <iyersrinivas1@gmail.com>
2017-01-19 18:01:21 +0000

    updated with bing api

---
+++ index.js
@@ -0,0 +1,57 @@
+var express = require("express")();
+var mongo = require("mongodb").MongoClient;
+
+var request = require("request");
+
+
+express.get('/api/imagesearch/:searchParam', function(req, res) {
+
+// Call the bing image search API through node - image URLs, ALT text and page URL
+
+//var postUrl = "https://www.microsoft.com/cognitive-services/Demo/BingApisDemo/SearchAsync?path=v5.0/images/search%20&textFormat=html&textdecorations=true&mkt=en-us&safeSearch=Moderate&q="+searchParam;
+
+
+var urlGet = "https://api.cognitive.microsoft.com/bing/v5.0/images/search?q=" + req.params.searchParam;
+
+request({
+    url: urlGet,
+    method: 'GET',
+    headers: {
+        "Content-Type": "multipart/form-data",
+        "Ocp-Apim-Subscription-Key": "889f3e88c4dd47d48d644301d9172375"
+    }
+}, function(error, response, body) {
+    //  console.log(JSON.stringify(reqRes));
+    res.writeHead(200, {
+        "Content-Type": "text/json"
+    });
+
+
+    var fullResp = JSON.parse(response.body).value;
+    
+    fullResp = fullResp.map(function(val){
+        var imgUrl = ((val.contentUrl.split('&'))[5].replace(/[%]3a/g,':').replace(/[%]2f/g,'/').split('='))[1];
+        return ({
+            "imageUrl": imgUrl,
+            "alt": val.name,
+            "pageUrl": val.hostPageDisplayUrl
+        });
+         
+    });
+    
+
+    res.write(JSON.stringify(fullResp));
+    res.end();
+});
+
+
+// Store the req data in mongo
+
+});
+
+express.get('/api/latest/imagesearch', function(req, res) {
+    // query mongo and return the latest 10 results
+
+});
+
+express.listen(process.env.PORT || 8080);
